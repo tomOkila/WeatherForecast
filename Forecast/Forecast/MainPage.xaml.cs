7692e7997d1972642c3757275f7aa7745c322c53
TOM OKILA <TOM OKILA@METICSBUZ>
2016-10-12 11:07:10 +0300

    This is forecast code sample.

---
+++ Forecast/Forecast/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using CurrentWeather;
+using Forecast.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Forecast
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<ForecastList> ForeCast;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ForeCast = new ObservableCollection<ForecastList>();
+        }
+
+       
+
+        private  void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            WeatherFacade.PopulateWeatherDetails(ForeCast);
+            
+        }
+    }
+}
---
+++ Forecast/Forecast/WeatherFacade.cs
@@ -0,0 +1,57 @@
+﻿using CurrentWeather;
+using Forecast.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Forecast
+{
+    public class WeatherFacade
+    {
+        public async static Task<RootObject> GetWeatherDetails(double lat, double lon)
+        {
+            var http = new HttpClient();
+            var url = String.Format("http://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&appid=d7d3346a960174c8076375c099739df7&cnt=5&units=metric", lat, lon);
+            var response = await http.GetAsync(url);
+            var jsonMessage = await response.Content.ReadAsStringAsync();
+
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonMessage));
+
+            var result = (RootObject)serializer.ReadObject(ms);
+            return result;
+        }
+
+
+        public async static void PopulateWeatherDetails(ObservableCollection<ForecastList> foreCast)
+        {
+
+            var position = await LocationManager.GetPosition();
+
+            var lat = position.Coordinate.Point.Position.Latitude;
+            var lon = position.Coordinate.Point.Position.Longitude;
+
+
+            var weatherDetails = await GetWeatherDetails(lat, lon);
+            var weatherResults = weatherDetails.list;
+            var ImageResults = weatherResults;
+            foreach (var data in ImageResults)
+            {
+                foreCast.Add(data);
+                
+            }
+
+           
+
+
+        }
+
+    }
+}
